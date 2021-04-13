158e2edb92c10a54088cfa3b0da20969639da32f
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-13 16:20:43 +0200

    primo step

--- myScript.js
+++ myScript.js
@@ -5,4 +5,14 @@
 3. stampa la lista ordinata alfabeticamente
 4. scrivi anche la posizione "umana" (partendo da 1) 
 della lista in cui il nuovo utente si trova */
- 
+
+var cognomeUtente = prompt("Inserisci il tuo cognome");
+var arrayCognomi = ["Neri", "Bianco", "Rossi", "Fumagalli", "Gialli", "Gallo"];
+var listaOrdinataCognomi = [];
+// Inserisce cognomeUtente nell array
+arrayCognomi.push(cognomeUtente);
+
+for (var i = 0;i<arrayCognomi.length){
+    
+}   
+
