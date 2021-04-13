158884473fd79642759acc0ba2e97a3aa5e61299
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-13 17:19:30 +0200

    Completa esercizio

--- myScript.js
+++ myScript.js
@@ -7,11 +7,23 @@
 della lista in cui il nuovo utente si trova */
 
 var cognomeUtente = prompt("Inserisci il tuo cognome");
-var arrayCognomi = ["Azzurri","Neri", "Bianco", "Rossi", "Fumagalli", "Gialli", "Gallo","Zinco"];
+var arrayCognomi = ["Azzurri", "Neri", "Bianco", "Rossi", "Fumagalli", "Gialli", "Gallo", "Zinco"];
 var listaOrdinataCognomi = [];
 var minAlfabetico;
+var posizioneCognomeUtente;
+
 // Inserisce cognomeUtente nell array
 arrayCognomi.push(cognomeUtente);
-arrayCognomi.sort(); //Ordina l'array
+
+//Ordina l'array
+arrayCognomi.sort();
+
+//Stampa l'array ordinato
 console.log(arrayCognomi);
+
+//Scrive la posizione "umana" del cognome utente
+var i = 0;
+trovato = false;
+posizioneCognomeUtente = arrayCognomi.indexOf(cognomeUtente)+1;
+console.log("il cognome dell'utente è a posizione " + posizioneCognomeUtente);
 
