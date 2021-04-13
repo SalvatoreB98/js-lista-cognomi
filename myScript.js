766c5375cb7a0ecfc60424e597b99fa150b5bb1b
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-13 17:49:54 +0200

    Completa esercizio

--- myScript.js
+++ myScript.js
@@ -7,12 +7,14 @@
 della lista in cui il nuovo utente si trova */
 
 var cognomeUtente = prompt("Inserisci il tuo cognome");
-var arrayCognomi = ["Azzurri", "Neri", "Bianco", "Rossi", "Fumagalli", "Gialli", "Gallo", "Zinco"];
+var arrayCognomi = ["Azzurri", "Neri", "Bianco", "Rossi", "Fumagalli", "Gialli", "Gallo", "Zinco","Costanzo", "Arcuri", "Marino", "Sirianni"];
 var listaOrdinataCognomi = [];
 var minAlfabetico;
 var posizioneCognomeUtente;
+var elemento;
 
 // Inserisce cognomeUtente nell array
+cognomeUtente= cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
 arrayCognomi.push(cognomeUtente);
 
 //Ordina l'array
@@ -20,10 +22,17 @@ arrayCognomi.sort();
 
 //Stampa l'array ordinato
 console.log(arrayCognomi);
+elemento=document.getElementById("lista");
 
-//Scrive la posizione "umana" del cognome utente
+// Inietta l'array nel DOM
+for (var i = 0; i < arrayCognomi.length; i++) {
+    elemento.innerHTML += "<li>" + arrayCognomi[i] +"</li>";
+}
+
+//Scrive la posizione "umana" del cognome utente e la inietta nel DOM
 var i = 0;
 trovato = false;
 posizioneCognomeUtente = arrayCognomi.indexOf(cognomeUtente)+1;
 console.log("il cognome dell'utente è a posizione " + posizioneCognomeUtente);
+document.getElementById("element-position").innerHTML = "La posizione del cognome dell'utente è la " + posizioneCognomeUtente +"°"; 
 
